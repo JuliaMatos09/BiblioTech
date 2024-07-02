ae2fb7ae10981173a0124d02c8df8e8cea8d29ed
Julia Matos <julia.soares@sptech.school>
2024-07-02 15:00:50 -0300

    v-1.0 spring boot

---
+++ bibliotech/src/main/java/bibliotechback/service/UsuarioService.java
@@ -0,0 +1,27 @@
+package bibliotechback.service;
+
+import bibliotechback.entity.Usuario;
+import bibliotechback.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> findByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public void delete(int id) {
+        usuarioRepository.deleteById(id);
+    }
+}
